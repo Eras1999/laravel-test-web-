9341b6c3373e71ef8e8f408e362a063a10089ca2
Eranda Madusanka <madushankaeranda56@gmail.com>
2025-07-20 14:31:14 +0530

    chatbot added

--- web-app/resources/views/frontend/layouts/master.blade.php
+++ web-app/resources/views/frontend/layouts/master.blade.php
@@ -29,6 +29,19 @@
     @yield('styles')
 </head>
 <body>
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/687c976c64aaaf19121e9ec3/1j0jav0ia';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
     <!-- Preloader -->
     <div id="preloader">
         <img src="{{ asset('frontend/img/preloader.gif') }}" alt="">
